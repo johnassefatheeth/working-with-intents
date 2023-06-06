38136918ac1dc75b1446548101b38143d1dd2c74
johnassefatheeth <johnassefaforbussines@gmail.com>
2023-06-06 05:41:57 -0700

    making the main method

--- main.py
+++ main.py
@@ -105,7 +105,20 @@ mappings = {
 
 
 
-assistant =GenericAssistant('intents.json')
+assistant =GenericAssistant('intents.json', intent_methods=mappings)
 assistant.train_model()
 
-assistant.request("how are you")
+
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            message = recognizer.recognize_google(audio)
+            message = message.lower()
+
+        assistant.request(message)
+
+    except speech_recognition.UnknownValueError:
+        recognizer= speech_recognition.Recognizer()
