2d5ac00815cacec5141b1362c6a8fafd82f3f79d
johnassefatheeth <johnassefaforbussines@gmail.com>
2023-06-06 05:16:30 -0700

    creating file mthod

--- main.py
+++ main.py
@@ -11,7 +11,42 @@ speaker.setProperty('rate', 150 )
 
 todo_list=['go home','go church','record video']
 
+def create_note():
+    global recognizer
 
+    speaker.say("what should i write on your note?")
+    speaker.runAndWait()
+
+    done= False
+
+    while not done:
+        try:
+
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                note =recognizer.recognize_google(audio)
+                note =note.lower()
+
+                speaker.say("choose a filename")
+                speaker.runAndWait()
+
+                recognizer.adjust_for_ambient_noise(mic,duration=0.2)
+                audio =recognizer.listen(mic)
+
+                filename= recognizer.recognize_google(audio)
+                filename=filename.lower()
+
+            with open(filename,'w') as f:
+                f.write(note)
+                done = True
+                speaker.say(f"successfully opened file with the name {filename}")
+                speaker.runAndWait()
+        except speech_recognition.UnknownValueError:
+            recognizer= speech_recognition.Recognizer()
+            speaker.say("come again?")
+            speaker.runAndWait()
 
 
 assistant =GenericAssistant('intents.json')
