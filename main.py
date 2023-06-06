938c37761e0d6357ff9bb18c7c024d1b6e7cab67
johnassefatheeth <johnassefaforbussines@gmail.com>
2023-06-06 05:01:10 -0700

    correcting syntactic errors

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from neuralintetns import GenericAssistant
-import SpeechRecognition
+import speech_recognition
+from neuralintents import GenericAssistant
 import pyttsx3
 import sys
 
 
+recognizer = speech_recognition.Recognizer()
+
+speaker = pyttsx3.init()
+speaker.setProperty('rate', 150 )
+
+todo_list=['go home','go church','record video']
+
+
+
+
+assistant =GenericAssistant('intents.json')
+assistant.train_model()
+
+assistant.request("how are you")
